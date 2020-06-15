fb83e77e6390b46f4c271c08216539db2e70e788
Vansh Kapoor <42063201+Fanceh@users.noreply.github.com>
2020-06-15 17:53:22 +0530

    Product Page Update

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,12 @@ const selectElement = (s) => document.querySelector(s);
 
 //Open the menu on click
 selectElement('.open').addEventListener('click', () => {
-  selectElement('.nav-list').classList.add('active');
+    selectElement('.nav-list').classList.add('active');
 });
 
 //Close the menu on click
 selectElement('.close').addEventListener('click', () => {
-  selectElement('.nav-list').classList.remove('active');
+    selectElement('.nav-list').classList.remove('active');
 });
 
 //Search-Button on Click
@@ -22,49 +22,48 @@ selectElement('.close').addEventListener('click', () => {
 // });
 
 //featured-items buttons
-selectElement('.feature-men').addEventListener('click', () =>{
-  selectElement('.men-items').style.display="block";
-  selectElement('.women-items').style.display="none";
-  selectElement('.products-items').style.display="none";
+selectElement('.feature-men').addEventListener('click', () => {
+    selectElement('.men-items').style.display = "block";
+    selectElement('.women-items').style.display = "none";
+    selectElement('.products-items').style.display = "none";
 });
 
-selectElement('.feature-women').addEventListener('click', () =>{
-  selectElement('.men-items').style.display="none";
-  selectElement('.women-items').style.display="block";
-  selectElement('.products-items').style.display="none";
+selectElement('.feature-women').addEventListener('click', () => {
+    selectElement('.men-items').style.display = "none";
+    selectElement('.women-items').style.display = "block";
+    selectElement('.products-items').style.display = "none";
 });
 
-selectElement('.feature-products').addEventListener('click', () =>{
-  selectElement('.men-items').style.display="none";
-  selectElement('.women-items').style.display="none";
-  selectElement('.products-items').style.display="block";
+selectElement('.feature-products').addEventListener('click', () => {
+    selectElement('.men-items').style.display = "none";
+    selectElement('.women-items').style.display = "none";
+    selectElement('.products-items').style.display = "block";
 });
 
 //banners-btns
-selectElement('.circle-btn-1').addEventListener('click', () =>{
-  selectElement('.circle-btn-1').style.color="green";
-  selectElement('.circle-btn-2').style.color="black";
-  selectElement('.circle-btn-3').style.color="black";
-  selectElement('.banner-img1').style.display="block";
-  selectElement('.banner-img2').style.display="none";
-  selectElement('.banner-img3').style.display="none";
+selectElement('.circle-btn-1').addEventListener('click', () => {
+    selectElement('.circle-btn-1').style.color = "green";
+    selectElement('.circle-btn-2').style.color = "black";
+    selectElement('.circle-btn-3').style.color = "black";
+    selectElement('.banner-img1').style.display = "block";
+    selectElement('.banner-img2').style.display = "none";
+    selectElement('.banner-img3').style.display = "none";
 });
 
-selectElement('.circle-btn-2').addEventListener('click', () =>{
-  selectElement('.circle-btn-1').style.color="black";
-  selectElement('.circle-btn-2').style.color="green";
-  selectElement('.circle-btn-3').style.color="black";
-  selectElement('.banner-img1').style.display="none";
-  selectElement('.banner-img2').style.display="block";
-  selectElement('.banner-img3').style.display="none";
+selectElement('.circle-btn-2').addEventListener('click', () => {
+    selectElement('.circle-btn-1').style.color = "black";
+    selectElement('.circle-btn-2').style.color = "green";
+    selectElement('.circle-btn-3').style.color = "black";
+    selectElement('.banner-img1').style.display = "none";
+    selectElement('.banner-img2').style.display = "block";
+    selectElement('.banner-img3').style.display = "none";
 });
 
-selectElement('.circle-btn-3').addEventListener('click', () =>{
-  selectElement('.circle-btn-1').style.color="black";
-  selectElement('.circle-btn-2').style.color="black";
-  selectElement('.circle-btn-3').style.color="green";
-  selectElement('.banner-img1').style.display="none";
-  selectElement('.banner-img2').style.display="none";
-  selectElement('.banner-img3').style.display="block";
+selectElement('.circle-btn-3').addEventListener('click', () => {
+    selectElement('.circle-btn-1').style.color = "black";
+    selectElement('.circle-btn-2').style.color = "black";
+    selectElement('.circle-btn-3').style.color = "green";
+    selectElement('.banner-img1').style.display = "none";
+    selectElement('.banner-img2').style.display = "none";
+    selectElement('.banner-img3').style.display = "block";
 });
-
